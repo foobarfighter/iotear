42a7443c690bb0274d21a5789a8d90d066fa5964
Robert Remeika <bob.remeika@gmail.com>
2009-03-05 00:31:14 -0800

    Created a deadlock example without the use of sleep

--- scratchpad/deadlock_example.rb
+++ scratchpad/deadlock_example.rb
@@ -6,25 +6,40 @@ main_mutex = Mutex.new
 a_mutex = Mutex.new
 b_mutex = Mutex.new
 
+a_cv = ConditionVariable.new
+b_cv = ConditionVariable.new
+
+# The execution order of t1 and t2 does not matter, they will always create a deadlock
+
 t1 = Thread.new do
   b_mutex.synchronize {
-    sleep 0.01
-    puts "t1: trying to get lock on a_mutex"
+    puts "t1: in b"
+    puts "t1: trying to get a"
     a_mutex.synchronize {
-      puts "never gonna get it"
+      puts "t1: waiting on a"
+      a_cv.wait(a_mutex)
+      puts "t1: done waiting on a"
     }
   }
+  puts "t1: out of b"
 end
 
 t2 = Thread.new do
   a_mutex.synchronize {
-    sleep 0.01
-    puts "t2: trying to get lock on b_mutex"
+    puts "t2: in a"
+    puts "t2: trying to get b"
     b_mutex.synchronize {
-      puts "oh you're never gonna get it"
+      puts "t2: waiting on b"
+      b_cv.wait(b_mutex)
+      puts "t2: done waiting on b"
     }
   }
+  puts "t2: out of b"
 end
+
+# Sleep here to demonstrate that the lock can't be broken
+# even if the main process waits for a while
+sleep 2
 
 t1.join
 t2.join
