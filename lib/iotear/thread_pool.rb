792fb752eb42bca45eaa6ce24965ae8bbed853e1
Robert Remeika <bob.remeika@gmail.com>
2009-03-12 20:52:32 -0700

    Added some more specs; This is a safe saving point before I refactor the ThreadPool initialization

--- lib/iotear/thread_pool.rb
+++ lib/iotear/thread_pool.rb
@@ -1,7 +1,7 @@
 require 'thread'
 
 class ThreadPool
-  attr_reader :threads
+  attr_reader :threads, :mutex, :cv
 
   DEFAULT_THREAD_PREFIX = "threadpool"
   DEFAULT_BLOCK_ON_EXHAUST = false
@@ -16,12 +16,13 @@ class ThreadPool
 
   def initialize(thread_count, options = {})
     @threads ||= []
+    @thread_data ||= {}
 
     main_mutex = Mutex.new
     main_cv = ConditionVariable.new
 
-    creator_mutex = Mutex.new
-    creator_cv = ConditionVariable.new
+    @mutex = Mutex.new
+    @cv = ConditionVariable.new
 
     # Serially wait for each thread to spawn
     main_mutex.synchronize do
@@ -29,16 +30,20 @@ class ThreadPool
       (1..thread_count).each do |i|
         @threads << Thread.new do
           # Lock owner for spawned thread to wait on
-          creator_mutex.synchronize do
+          @mutex.synchronize do
             # Wait until main thread has released it's lock before spawning
             main_mutex.synchronize do
+              # Initialize data that will be available to main thread
               Thread.current[:name] = (options[:thread_prefix] || DEFAULT_THREAD_PREFIX) + i.to_s
               # Signal main thread that it's ok spawn
               main_cv.signal
             end
             # Put spawned thread into waiting state
-            creator_cv.wait(creator_mutex)
+            @cv.wait(@mutex)
           end
+
+          Thread.current[:task]
+          Thread.current[:args]
         end
         # Ensures that main thread won't run until it receives a signal that it's ok
         main_cv.wait(main_mutex)
@@ -52,9 +57,18 @@ class ThreadPool
     end
   end
 
+  def process(*args, &block)
+#    mutex.synchronize do
+#
+#      Thread.current[:task] = block
+#      Thread.current[:args] = args
+#    end
 
-  def process(&block)
     yield unless block.nil?
   end
 
+  private
+  def find_next_ready_thread
+    
+  end
 end
--- spec/thread_pool_spec.rb
+++ spec/thread_pool_spec.rb
@@ -2,21 +2,49 @@ require "#{File.dirname(__FILE__)}/spec_helper.rb"
 require 'thread'
 
 describe ThreadPool do
-  attr_reader :thread_count, :pool
+  attr_reader :thread_count, :pool, :thread_names
+
+  def kill_threads(pool)
+    pool.threads.each do |thread|
+      thread.kill!
+    end
+  end
 
   before do
     @thread_count = 10
     @pool = ThreadPool.new(@thread_count)
+    @thread_names = pool.threads.collect { |thread| thread[:name] }
+    thread_names.size.should == thread_count
+  end
+
+  after do
+    kill_threads(pool)
   end
 
   context "#initialize" do
+    it "sets cv" do
+      pool.cv.should_not be_nil
+    end
+
+    it "sets the threads" do
+      pool.threads.should_not be_nil
+      pool.threads.size.should == 10
+    end
+
     it "creates n number of sleeping threads" do
       pool.threads.find_all { |thread| thread.status == "sleep" }.size.should == thread_count
     end
 
     describe "when a thread prefix is passed" do
-      it "gives each thread a default name" do
+      before do
         @pool = ThreadPool.new(@thread_count, :thread_prefix => "foo")
+      end
+
+      after do
+        kill_threads(pool)
+      end
+      
+      it "gives each thread a default name" do
         pool.threads.each do |thread|
           thread[:name].should match(/foo\d+/)
         end
@@ -38,16 +66,16 @@ describe ThreadPool do
       attr_reader :one_thread_working
       
       it "wakes up a thread to process" do
-        pending
-        @one_thread_working = false
-        def test_thread_status
-          pool.threads.each do |thread|
-            puts thread.status
-            puts thread[:name]
-          end
+        @threads_working = 0
+        def sleepy
+          sleep 1 # Should be enough time to see if all threads are working
         end
-        pool.process { test_thread_status }
-        one_thread_working.should be_true
+        pool.process { sleepy }
+        
+        pool.threads.each do |thread|
+          @threads_working += 1 if thread[:status] == 'run'
+        end
+        @threads_working.should == 1
       end
     end
 
